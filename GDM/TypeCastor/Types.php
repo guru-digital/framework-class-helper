9a9155f1fb822a5a9f41e92b8b160eeee56dd08c
Corey Sewell <corey@gdmedia.tv>
2015-08-04 11:43:24 +1200

    Cleanup

--- GDM/TypeCastor/Types.php
+++ GDM/TypeCastor/Types.php
@@ -18,24 +18,30 @@ class Types {
 	public static function GetType($type) {
 		$result		 = null;
 		$cleanType	 = preg_replace('/\[\]$/', "", trim($type));
-		if (isset(static::$aliases[$type])) {
-			$type = static::$aliases[$type];
+		if (isset(static::$aliases[$cleanType])) {
+			$cleanType = static::$aliases[$cleanType];
 		}
-//		echo '<pre class="debug"> "$cleanType" ' . print_r($cleanType, true)  . '</pre>';
-//		echo '<pre class="debug"> "class_exists(' . $cleanType . ') ' . print_r(class_exists($cleanType) ? "Yes" : "No", true)  . '</pre>';
-//		echo '<pre class="debug"> "' . $cleanType . ' instanceof \GDM\Interfaces\Createable" ' . print_r($cleanType instanceof \GDM\Interfaces\Createable ? "Yes" : "No", true)   . '</pre>';
-//		echo '<pre class="debug"> " is_subclass_of(' . $cleanType . ', \GDM\Interfaces\Createable)" ' . print_r(is_subclass_of($cleanType, '\GDM\Interfaces\Createable') ? "Yes" : "No", true) . PHP_EOL . '</pre>';
 
-		if (class_exists($cleanType) && ($cleanType instanceof \GDM\Interfaces\Createable || is_subclass_of($cleanType, '\GDM\Interfaces\Createable'))) {
-			$result = self::Createable;
-		} else {
-			foreach (ClassInfo::create(__CLASS__)->getConstants() as $value) {
-				if (strtolower($cleanType) === strtolower($value)) {
-					$result = $value;
-					break;
-				}
+		foreach (ClassInfo::create(__CLASS__)->getConstants() as $value) {
+			if (strtolower($cleanType) === strtolower($value)) {
+				$result = $value;
+				break;
 			}
 		}
+
+//		if (is_null($result)) {
+//			echo '<pre class="debug"> "$cleanType" ' . print_r($cleanType, true) . '</pre>';
+//			echo '<pre class="debug"> "class_exists(' . $cleanType . ') ' . print_r(class_exists($cleanType) ? "Yes" : "No", true) . '</pre>';
+//			echo '<pre class="debug"> "' . $cleanType . ' instanceof \GDM\Interfaces\Createable" ' . print_r($cleanType instanceof \GDM\Interfaces\Createable ? "Yes" : "No", true) . '</pre>';
+//			echo '<pre class="debug"> " is_subclass_of(' . $cleanType . ', \GDM\Interfaces\Createable)" ' . print_r(is_subclass_of($cleanType, '\GDM\Interfaces\Createable') ? "Yes" : "No", true) . PHP_EOL . '</pre>';
+//		}
+		if (is_null($result) && class_exists($cleanType) && ($cleanType instanceof \GDM\Interfaces\Createable || is_subclass_of($cleanType, '\GDM\Interfaces\Createable'))) {
+			$result = self::Createable;
+		}
+		if (is_null($result)) {
+			throw new \Exception('"' . $type . '" is not a valid ' . __CLASS__);
+		}
+
 		return $result;
 	}
 
